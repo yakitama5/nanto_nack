0538d537e4ce179085c8666a6f8c06946da81891
Yakuran <rittitarou@yahoo.co.jp>
2026-03-28 10:49:58 +0900

    ♻️generate: audioplayers追加に伴う各プラットフォームのプラグイン登録ファイルを更新
    linux/macos/windows の generated_plugin_registrant および generated_plugins.cmake に
    AudioplayersPlugin を登録（melos bootstrap / flutter pub get の自動生成結果）。

--- apps/app_main/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/app_main/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import audioplayers_darwin
 import geolocator_apple
 import sqlite3_flutter_libs
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
 }
