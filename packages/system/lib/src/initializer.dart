a20b603b29670a231c4072431ccc9c34f4d355ca
Yakuran <rittitarou@yahoo.co.jp>
2026-04-06 15:55:09 +0900

    🔧chore: AppInitializer に Crashlytics と RemoteConfig の初期化処理を追加
    Firebase 初期化後に以下を実行するよう AppInitializer を更新。
    - CrashlyticsService.setupFlutterErrorHandling(): Flutter/Dart エラーを Crashlytics に接続
    - RemoteConfigService.initialize(): デフォルト値の設定とリモート設定の取得・反映

--- packages/system/lib/src/initializer.dart
+++ packages/system/lib/src/initializer.dart
@@ -1,10 +1,14 @@
 import 'package:system/src/environment/app_environment.dart';
+import 'package:system/src/firebase/crashlytics_service.dart';
 import 'package:system/src/firebase/firebase_service.dart';
+import 'package:system/src/firebase/remote_config_service.dart';
 
 class AppInitializer {
   const AppInitializer._();
 
   static Future<void> initialize() async {
     await FirebaseService.initialize(AppEnvironment.flavor);
+    CrashlyticsService.setupFlutterErrorHandling();
+    await RemoteConfigService.initialize();
   }
 }
