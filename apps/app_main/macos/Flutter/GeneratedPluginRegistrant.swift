8f0f52e37e9c59149de3eb09089838f53b46146d
Yakuran <rittitarou@yahoo.co.jp>
2026-04-06 18:29:36 +0900

    ♻️generete: macOS プラグイン登録に Firebase Analytics/Crashlytics/RemoteConfig を追加
    firebase_analytics・firebase_crashlytics・firebase_remote_config パッケージ追加に伴い、
    flutter pub get / melos bootstrap により自動更新された生成ファイル

--- apps/app_main/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/app_main/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,10 @@ import FlutterMacOS
 import Foundation
 
 import audioplayers_darwin
+import firebase_analytics
 import firebase_core
+import firebase_crashlytics
+import firebase_remote_config
 import geolocator_apple
 import package_info_plus
 import shared_preferences_foundation
@@ -15,7 +18,10 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  FirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FirebaseAnalyticsPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
+  FirebaseRemoteConfigPlugin.register(with: registry.registrar(forPlugin: "FirebaseRemoteConfigPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
